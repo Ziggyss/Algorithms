ca85f30f2224ed190d76cd29ee88a0bb3f31d8b1
Lisa Wilton <mishmao14@gmail.com>
2020-02-19 13:04:14 +0000

    lisa-wilton

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,22 @@
 
 import argparse
 
+# test = [100, 90, 80, 50, 20, 10]
+
 def find_max_profit(prices):
-  pass
+  for i in range (0, len(prices)):
+    max_value = max(prices)
+    max_index = prices.index(max_value)
+    if max_index > 0:
+        min_value = min(prices[0:max_index]) 
+        result = max_value - min_value
+    else:
+      result = prices[1] - prices[0]    
+    return result   
+
+# This is my first pass solution, but it doesn't work for the fourth test. I misunderstood what needed to be done with edge cases where the max value is at the beginning of the array. Now working on a better solution.
+  
+# print(find_max_profit(test))
 
 
 if __name__ == '__main__':
